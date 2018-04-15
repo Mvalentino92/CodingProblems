3b8b0889cb39a636e0d0a14ceea063c9ca57705e
Michael Valentino <michael.valentino92@gmail.com>
2018-04-15 17:40:11 -0400

    Add files via upload

---
+++ Project_Euler_a72.java
@@ -0,0 +1,71 @@
+import java.math.*;
+public class Project_Euler_a72
+{
+	public static int bound = 1000000;
+	public static int[] primes = sieve(bound);
+	
+	//Returns a list of the primes underneath the bound.
+	public static int[] sieve(int bound)
+	{
+		//Initialize the array up to the bound.
+		int[] numbers = new int[bound+ 1];
+		for(int i = 0; i < numbers.length; i++) numbers[i] = i;
+
+		//Apply the sieve and kill any non prime numbers by replacing them with a 0.
+		int killCount = 2;
+		for(int i = 2; i <= (int)Math.sqrt(bound); i++)
+		{
+			for(int j = i + i; j < numbers.length; j += i) 
+			{
+				if(numbers[j] != 0)
+				{
+					numbers[j] = 0;
+					killCount++;
+				}
+			}
+		}
+		
+		//Put the remaining primes into an array and return.
+		int primes[] = new int[numbers.length - killCount];
+		int index = 0;
+		for(int i = 0; i < numbers.length; i++)
+		{
+			if(numbers[i] > 1) primes[index++] = i;
+		}
+		return primes;
+	}
+	
+	//Returns the number of totients using prime factors of the number.
+	public static int totient(int number)
+	{
+		//Keep track of current index, update tote, and track the prime factors for uniqueness.
+		int index = 0;
+		double tote = number;
+		int lastNumber = 0;
+
+		//Keep grabbing prime factors and using them if they are unique.
+		while(number != 1)
+		{
+			int remainder = number % primes[index];
+			while(remainder != 0) remainder = number % primes[++index];
+			number /= primes[index];
+			if(primes[index] != lastNumber) 
+			{
+				tote *= (1 - 1.0/primes[index]);
+				lastNumber = primes[index];
+			}
+		}
+		return (int)tote;
+	}
+
+	public static void main(String[] args)
+	{
+		BigInteger count = BigInteger.ZERO;
+		for(int i = 2; i <= bound; i++)
+		{
+			BigInteger toAdd = BigInteger.valueOf(totient(i));
+			count = count.add(toAdd);
+		}
+		System.out.println(count);
+	}
+}
