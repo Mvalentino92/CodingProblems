e2ddba2b157e5a9107120b8d6964b55e99a2038c
Michael Valentino <michael.valentino92@gmail.com>
2018-05-11 08:49:47 -0400

    Add files via upload

---
+++ Project_Euler_a45.hs
@@ -0,0 +1,32 @@
+common :: Integral a => [a] -> [a] -> [a]
+common _ [] = []
+common ls (h:hs)
+            | null ps = common ls hs
+            | head ps /=  h = common ps hs
+            | otherwise = head ps : common ps hs
+            where ps = dropWhile (<h) ls
+
+
+tri :: Integral a => a -> [a]
+tri n = v : tri (n+1)
+        where v = (n^2 + n) `div` 2
+
+pen :: Integral a => a -> [a]
+pen n = v : pen (n+1)
+        where v = (3*n^2 - n) `div` 2
+                 
+hex :: Integral a => a -> [a]
+hex n = v : hex (n+1)
+        where v = (2*n^2 - n)
+
+
+main = do
+       let b = 100000
+       let t = take b $ tri 1
+       let p = take b $ pen 1
+       let h = take b $ hex 1
+       let c = common t p
+       let cc = common h c
+       let sol = head $ dropWhile (<=40755) cc
+       putStr "The solution is: "
+       print sol
