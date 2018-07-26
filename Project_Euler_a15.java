72ee6596562e5dcb22398c9f21ef1b1d77204b1d
Michael Valentino <michael.valentino92@gmail.com>
2018-07-26 09:01:26 -0400

    Add files via upload

--- Project_Euler_a15.java
+++ Project_Euler_a15.java
@@ -7,10 +7,10 @@ public class Project_Euler_a15
 	public static void main(String[] args)
 	{
 		long[] arr = new long[21];
-		for(long i = 0; i < arr.length; i++) arr[(int)i] = i + 1;
+		for(int i = 0; i < arr.length; i++) arr[i] = (long)(i + 1);
 		for(int count = 2; count < arr.length; count++)
 		{
-			for(long i = 1; i < arr.length; i++) arr[(int)i] = arr[(int)i] + arr[(int)i-1];
+			for(int i = 1; i < arr.length; i++) arr[i] = arr[i] + arr[i-1];
 		}
 		System.out.println(arr[arr.length-1]);
 	}
