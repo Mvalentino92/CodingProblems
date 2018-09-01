7d12de42f606be17cc897be7b2913c57709931a8
Michael Valentino <michael.valentino92@gmail.com>
2018-08-31 21:07:22 -0400

    Add files via upload

--- Project_Euler_c205.jl
+++ Project_Euler_c205.jl
@@ -6,15 +6,15 @@ c = 6^6
 
 #Adding up peters counts
 for i = 1:4
-	for ii = 1:4
-		for iii = 1:4
-			for iiii = 1:4
-				for iiiii = 1:4
-					for iiiiii = 1:4
-						for iiiiiii = 1:4
-							for iiiiiiii = 1:4
-								for iiiiiiiii = 1:4
-									val = i + ii + iii + iiii + iiiii + iiiiii + iiiiiii + iiiiiiii + iiiiiiiii
+	for i2 = 1:4
+		for i3 = 1:4
+			for i4 = 1:4
+				for i5 = 1:4
+					for i6 = 1:4
+						for i7 = 1:4
+							for i8 = 1:4
+								for i9 = 1:4
+									val = i + i2 + i3 + i4 + i5 + i6 + i7 + i8 + i9
 									peters[val-8] += 1
 								end
 							end
@@ -28,12 +28,12 @@ end
 
 #Adding up colins counts
 for j = 1:6
-	for jj = 1:6
-		for jjj = 1:6
-			for jjjj = 1:6
-				for jjjjj = 1:6
-					for jjjjjj = 1:6
-						val = j + jj + jjj + jjjj + jjjjj + jjjjjj 
+	for j2 = 1:6
+		for j3 = 1:6
+			for j4 = 1:6
+				for j5 = 1:6
+					for j6 = 1:6
+						val = j + j2 + j3 + j4 + j5 + j6 
 						colins[val - 5] += 1
 					end
 				end
